b48492064f70a2659362ffdadc44ba12cc3496a4
t-t-001 <t_toshiyuki@td5.so-net.ne.jp>
2022-07-18 22:12:33 +0900

    4日目Part2 オープニング画面作成

--- img_resize.py
+++ img_resize.py
@@ -2,4 +2,4 @@ from PIL import Image
 
 img = Image.open('./images/spaceRockets_001.png')
 img_resize = img.resize((50,100))
-img_resize.save('./images/spaceRockets_001.png')
+img_resize.save('./images/rocket.png')
--- opening.py
+++ opening.py
@@ -5,4 +5,19 @@ WIDTH  = 400
 HEIGHT = 600
 
 rocket = Actor("rocket",center=(200,300))
+def draw():
+    screen.clear()
+    for i in range(20):
+        rect = Rect((random.randrange(WIDTH),random.randrange(HEIGHT)),(2,2))
+        screen.draw.rect(rect,'WHITE')
+    for i in range(10):
+        screen.draw.circle(rocket.midbottom,i+1,(255,i*20,0))
+    for i in range(50):
+        screen.draw.line((150-i*3,550+i),(250+i*3,550+i),(128,128,128))
+    #screen.draw.text("Moon Lander",(50,100),owidth=1.5,ocolor=(255,255,0),color=(0,0,0),fontsize=64)
+    screen.draw.text("Moon Lander",(50,100),owidth=1.5,ocolor=('GREEN'),color=(0,0,0),fontsize=64)
+    rocket.draw()
+pgzrun.go()
+
+
 
